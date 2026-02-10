ce2b45a71611eac244a8ffd9b0c38f7d5a38c144
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2026-02-10 23:51:58 +0000

    Initial commit, boilerplate code working

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
+#include <GLFW/glfw3.h>
+
+#include <experimental/meta>
+#include <print>
+
+struct foo
+{};
+
+void glfw_error_callback(int error, const char* description)
+{
+    std::println("GLFW error {}: {}", error, description);
+}
+
+int main()
+{
+    glfwSetErrorCallback(glfw_error_callback);
+
+    if (!glfwInit()) {
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui + GLFW", nullptr, nullptr);
+    if (!window) {
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    //glfwSwapInterval(1); // vsync
+    std::println("{}\n", std::meta::display_string_of(^^foo));
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Hello");
+        ImGui::Text("ImGui is working!");
+        ImGui::Text("FPS: %.1f", io.Framerate);
+        ImGui::End();
+        ImGui::Render();
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
